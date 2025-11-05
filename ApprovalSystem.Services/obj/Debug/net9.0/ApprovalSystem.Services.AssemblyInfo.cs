9528ae1a4e6a689872adf501003ae6120ee5c78a
Abdulrahman <apsproye@gmail.com>
2025-11-05 03:44:39 +0300

    دمج

--- ApprovalSystem.Services/obj/Debug/net9.0/ApprovalSystem.Services.AssemblyInfo.cs
+++ ApprovalSystem.Services/obj/Debug/net9.0/ApprovalSystem.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApprovalSystem.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+156c85dda74ec973889c12f22d5efd5a9d0f9da0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApprovalSystem.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApprovalSystem.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
